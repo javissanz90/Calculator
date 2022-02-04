6efa955420e66955a3ac41c1f1aa72317a9c94ee
unknown <javi.ssanz90@gmail.com>
2022-02-04 10:19:05 +0100

    DTO validation

--- calculator/src/main/java/com/sanitas/dto/RequestDTO.java
+++ calculator/src/main/java/com/sanitas/dto/RequestDTO.java
@@ -2,8 +2,8 @@ package com.sanitas.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.groups.Default;
 
 import com.sanitas.exception.ConstantException;
 
@@ -15,15 +15,12 @@ import com.sanitas.exception.ConstantException;
 @Builder
 public class RequestDTO {
 
-    @NotNull(groups = {ValidationGroup.class}, message = ConstantException.OPERATION_MANDATORY)
+    @NotEmpty(message = ConstantException.OPERATION_MANDATORY)
     private String operation;
     
-    @NotNull(groups = {ValidationGroup.class}, message = ConstantException.OPERATOR1_MANDATORY)
+    @NotNull(message = ConstantException.OPERATOR1_MANDATORY)
     private Integer op1;
     
-    @NotNull(groups = {ValidationGroup.class}, message = ConstantException.OPERATOR2_MANDATORY)
+    @NotNull(message = ConstantException.OPERATOR2_MANDATORY)
     private Integer op2;
-
-    public static interface ValidationGroup extends Default {
-        /* Validation group */ }
 }
